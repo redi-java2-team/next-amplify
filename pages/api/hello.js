67a594aeec3267f96c3e6c1d17a791fe98ead1fc
MohamedElSherif <moh@moh.moh>
2022-11-19 15:14:52 +0100

    Unsgtable revlaidat commit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+// export default function handler(req, res) {
+//   res.status(200).json({ name: 'John Doe' })
+// }
+
+export default async function handler(req, res) {
+  // // Check for secret to confirm this is a valid request
+  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  //   return res.status(401).json({ message: 'Invalid token' });
+  // }
+
+  try {
+    await res.unstable_revalidate('/copy');
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating');
+  }
 }
