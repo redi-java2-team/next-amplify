8d652c983fa09336a2497526b68a4ceee0ec7b27
MohamedElSherif <moh@moh.moh>
2022-11-19 15:20:15 +0100

    Unsgtable revlaidat fix

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -11,11 +11,11 @@ export default async function handler(req, res) {
   // }
 
   try {
-    await res.unstable_revalidate('/copy');
+    await res.revalidate('/copy');
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send('Error revalidating');
+    return res.status(500).send('Error revalidating1',err);
   }
 }
